a36ee16ad82f1a62c59a254ca65de52bcfb9e727
ojaq <abangojaq@gmail.com>
2023-03-22 14:39:05 +0700

    added postowner middleware

--- routes/api.php
+++ routes/api.php
@@ -20,7 +20,7 @@ Route::middleware(['auth:sanctum'])->group(function (){
     Route::get('/posts', [PostController::class, 'index']);
     Route::get('/posts/{id}', [PostController::class, 'show']);
     Route::post('/posts', [PostController::class, 'store']);
-    Route::patch('/posts/{id}', [PostController::class, 'update']);
+    Route::patch('/posts/{id}', [PostController::class, 'update'])->middleware(['post.owner']);
 
     Route::get('/logout', [AuthController::class, 'logout']);
     Route::get('/me', [AuthController::class, 'me']);
