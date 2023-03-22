038291b1a12d4ca32239043814f3b2258e24234c
ojaq <abangojaq@gmail.com>
2023-03-22 15:01:52 +0700

    comment resource and middleware

--- routes/api.php
+++ routes/api.php
@@ -23,7 +23,9 @@ Route::middleware(['auth:sanctum'])->group(function (){
     Route::post('/posts', [PostController::class, 'store']);
     Route::patch('/posts/{id}', [PostController::class, 'update'])->middleware(['post.owner']);
     Route::delete('/posts/{id}', [PostController::class, 'delete'])->middleware(['post.owner']);
+    
     Route::post('/comment', [CommentController::class, 'store']);
+    Route::post('/comment/{id}', [CommentController::class, 'store'])->middleware(['comment.owner']);
 
     Route::get('/logout', [AuthController::class, 'logout']);
     Route::get('/me', [AuthController::class, 'me']);
