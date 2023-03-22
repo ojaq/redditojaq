30cdc8337270fda469d4af6067a5e5ee29cf1ffb
ojaq <abangojaq@gmail.com>
2023-03-22 14:56:33 +0700

    create comment

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\CommentController;
 use App\Http\Controllers\PostController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -22,6 +23,7 @@ Route::middleware(['auth:sanctum'])->group(function (){
     Route::post('/posts', [PostController::class, 'store']);
     Route::patch('/posts/{id}', [PostController::class, 'update'])->middleware(['post.owner']);
     Route::delete('/posts/{id}', [PostController::class, 'delete'])->middleware(['post.owner']);
+    Route::post('/comment', [CommentController::class, 'store']);
 
     Route::get('/logout', [AuthController::class, 'logout']);
     Route::get('/me', [AuthController::class, 'me']);
