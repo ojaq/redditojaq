93bacc42c4c1ed84291f11c0476fc2e368ac0c35
ojaq <abangojaq@gmail.com>
2023-03-22 14:08:58 +0700

    added register

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -6,6 +6,7 @@ use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
 use Illuminate\Validation\ValidationException;
 
 class AuthController extends Controller
@@ -43,5 +44,35 @@ class AuthController extends Controller
     public function me(){
         $user = Auth::user();
         return response()->json($user);
+    }
+
+    public function register(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'email' => 'required|email|unique:users',
+            'password' => 'required|min:8|confirmed',
+            'username' => 'required|unique:users',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 422);
+        }
+
+        $user = User::create([
+            'email' => $request->email,
+            'password' => bcrypt($request->password),
+            'username' => $request->username,
+        ]);
+
+        if($user) {
+            return response()->json([
+                'success' => true,
+                'user'    => $user,  
+            ], 201);
+        }
+
+        return response()->json([
+            'success' => false,
+        ], 409);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -20,6 +20,7 @@ Route::get('/posts', [PostController::class, 'index'])->middleware('auth:sanctum
 Route::get('/posts/{id}', [PostController::class, 'show'])->middleware('auth:sanctum');
 
 
+Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 Route::get('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
 Route::get('/me', [AuthController::class, 'me'])->middleware('auth:sanctum');
